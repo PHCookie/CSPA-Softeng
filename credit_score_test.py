d386f1cdae40e1bf944fc66e24463529adbc8264
PHCookie <100187117+PHCookie@users.noreply.github.com>
2022-05-29 16:37:19 +0800

    Update credit_score_test.py

--- credit_score_test.py
+++ credit_score_test.py
@@ -3,11 +3,31 @@ import pandas as pd
 
 # Columns: Loan ID,Customer ID,Loan Status,Current Loan Amount,Term,Credit Score,Annual Income,Years in current job,Home Ownership,Purpose,Monthly Debt,Years of Credit History,Months since last delinquent,Number of Open Accounts,Number of Credit Problems,Current Credit Balance,Maximum Open Credit,Bankruptcies,Tax Liens
 dataset = pd.read_csv('data/credit_train.csv')
-print(dataset[dataset["Loan Status"] == "Fully Paid"])
 
-# Calcuation:
-# Payment history (35%)
-# Accounts owed (30%)
-# Length of credit history (15%)
-# Credit mix (10%)
-# New credit (10%)
+#Remove unwanted values "999999999"
+clean_dataset = dataset[dataset["Current Loan Amount"] < 99999999 ]
+
+#Paid and Chargedoff data
+paid_dataset = clean_dataset[dataset["Loan Status"] == "Fully Paid"]
+chargedoff_dataset = clean_dataset[dataset["Loan Status"] == "Charged Off"]
+
+# Calculation:
+# --------Payment history (35%)--------
+
+# ------Accounts owed (30%)-----------
+account_owed = clean_dataset["Current Loan Amount"]
+#print(account_owed)
+
+#------Length of credit history (15%)-------------
+credit_history = clean_dataset["Years of Credit History"]
+#print(credit_history)
+
+
+#loop to get chistory on each spec row
+#for x in range(100000):
+    #ch = credit_history.iloc[x]
+    #print(ch)
+
+# ---------Credit mix (10%)------------
+
+# ---------New credit (10%)------------
