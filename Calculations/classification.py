6675121f5cf1db20849a66972fe071d210442f15
Samantha Bayaga <100513376+Sammily-NN@users.noreply.github.com>
2023-02-10 03:06:40 +0300

    Update classification.py
    Editing Score filter range, using headers instead of pandas.drop to retrieve columns.

--- Calculations/classification.py
+++ Calculations/classification.py
@@ -1,36 +1,75 @@
 import pandas as pd
 
-    # Using Random Forest Generates a 67% accuracy.
+# Using Machine Learning 'Random Forest' for Score Classing -
+# Less transparent, accuracy 74% tested with online dataset
 def byRandomForest(dataset):
 
     from sklearn.model_selection import train_test_split
+    
+    # Used as the basis/FICO accurate data 
     df = pd.read_csv('data/FICODATA_modified.csv')
+    # USed as the current data to predict
     testingFrame = dataset
-
-    X = df.drop(["Customer ID", "FICO Score","Score","newcred"], axis=1)
+    
+    # Using Generated Credit Score as class indicator
+    header = ['payhis','amtowed','lenofcrhis','newcred','Score']
+    
+    X = df[header]
     y = df["FICO Score"]
 
-
+    # Processing Model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=14)
     from sklearn.ensemble import RandomForestClassifier
     rf_model = RandomForestClassifier(n_estimators=50, random_state=14)
     rf_model.fit(X_train, y_train)
 
-    #predictions = rf_model.predict(X_test)
     from sklearn import metrics
-    # Model Accuracy, how often is the classifier correct?
-    #print("Accuracy:",metrics.accuracy_score(y_test, predictions))
 
-
-    preddata = testingFrame.drop(["Customer ID", "Current Loan Amount",
-                                  "Months since last delinquent" ,"newcred", "Score"], axis=1)
-
-
+    # Dataset Class Score Prediction
+    preddata = testingFrame[header]
     result = rf_model.predict(preddata)
+    
+    #Saving to CSV
     testingFrame["Class Score"] = result
 
     return testingFrame
 
+
+
+# Using the'CSPA scoring' ranges for Score Classing -
+# More transparent, accuracy 66% tested with online dataset
+def byCSPA(dataset):
+
+        #######-----------SCORE CATEGORIZATION---------------########
+    #Filter  Credit Scores Greater than 0 and less than 850.(FICO score range)
+    filter_score = dataset[(dataset["Score"] > 0)]
+    
+    # Setting limit to scoring
+    filter_score = filter_score[(filter_score["Score"] > 0) & (filter_score["Score"] <= 900)] 
+
+    #5. Exceptional Credit Scores
+    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 820, filter_score['CSPA Score'].replace(0,"Exceptional"))
+
+    #4. VERY GOOD Credit Scores
+    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 760, filter_score['CSPA Score'].replace(0, "Very Good"))
+
+
+    #3. Good Credit Scores
+    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 620, filter_score['CSPA Score'].replace(0,"Good"))
+
+
+    #2. Fair Credit Scores
+    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 530, filter_score['CSPA Score'].replace(0,"Fair"))
+
+
+    #1. Poor Credit Scores
+    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] > 0, filter_score['CSPA Score'].replace(0,"Poor"))
+
+    return dataset
+
+
+
+# Count each class amount. Used only for Graphing
 def obtain(dataset):
 
     #1. Poor Credit Scores
@@ -51,33 +90,5 @@ def obtain(dataset):
     Total = [len(poor), len(fair), len(good), len(verygood),len(exceptional)]
     
     return Total
-
-
-def byCSPA(dataset):
-
-        #######-----------SCORE CATEGORIZATION---------------########
-    #Filter  Credit Scores Greater than 0 and less than 850.(FICO score range)
-    filter_score = dataset[(dataset["Score"] > 0)]
-    filter_score = filter_score[(filter_score["Score"] > 0) & (filter_score["Score"] <= 990)] 
-
-    #5. Exceptional Credit Scores
-    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 831, filter_score['CSPA Score'].replace(0,"Exceptional"))
-
-    #4. VERY GOOD Credit Scores
-    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 731, filter_score['CSPA Score'].replace(0, "Very Good"))
-
-
-    #3. Good Credit Scores
-    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 571, filter_score['CSPA Score'].replace(0,"Good"))
-
-
-    #2. Fair Credit Scores
-    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] >= 400, filter_score['CSPA Score'].replace(0,"Fair"))
-
-
-    #1. Poor Credit Scores
-    filter_score['Class Score'] = filter_score['CSPA Score'].mask(filter_score["Score"] > 0, filter_score['CSPA Score'].replace(0,"Poor"))
-
-    return dataset
 
 
